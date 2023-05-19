594cec482d75e884b0b88a1f5c876a111623691c
Kevin White <kevinpwhitejr@gmail.com>
2023-05-18 17:49:08 -0700

    setup jellyfin and retired emby. Update transmission files.

--- transmission/start_ovpn.sh
+++ transmission/start_ovpn.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 route add -net 10.10.10.0 netmask 255.255.255.0 gw 172.21.0.1 #Adds the local LAN to route to the container so the web url works. 
-
-#I need to add a second route, nbot listed here is the router for the OVPN Virtual IP. Do not want to disclose for security reasons. 
+route add -net 10.10.200.0 netmask 255.255.0.0 gw 172.21.0.1 #Add VPN to local LAN route
 
 #Note: you need to add ca /usr/app/ca.ipvanish.com.crt to all the ipvanish-US-Los-Angeles-lax..... files or the docker image will look for certs in /etc/openvpn/...
 openvpn --config /usr/app/ipvanish-US-Los-Angeles-lax-a06.ovpn --auth-user-pass /usr/app/.ipvlogin
---
+++ transmission/transmissionstartup.sh
@@ -0,0 +1,3 @@
+cd /home/kevin/docker/home-docker-server/transmission/
+
+sudo docker-compose up -d
